87d321f099a456753908809659b537fa1fede289
Farah Qandalaft <farahqandalaft@optimumpartners.co>
2022-08-23 11:04:58 -0400

    feat/functional : beuty color adjusting code

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-const Container = document.querySelector(".root");
-let state = {};
-const selectedColors = [];
+const container = document.querySelector(".main");
 const colors = {
   "#92c952": "Mantis",
   "#771796": "Seance",
@@ -13,6 +11,22 @@ const colors = {
   "#51aa97": "Tradewind",
   "#810b14": "Monarch",
 };
+const colorsHex = {
+  0: "#92c952",
+  1: "#771796",
+  2: "#24f355",
+  3: "#d32776",
+  4: "#f66b97",
+  5: "#56a8c2",
+  6: "#b0f7cc",
+  7: "#54176f",
+  8: "#51aa97",
+  9: "#810b14",
+};
+const selectedColors = [];
+let countMessage = "Count of selected cards : ";
+let selectedMessage = "Selected colors : ";
+let state = {};
 
 const fetchAPI = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/photos");
@@ -23,7 +37,7 @@ const fetchAPI = async () => {
 const createHeader = () => {
   const header = document.createElement("header");
   header.classList = "header-wrapper";
-  Container.appendChild(header);
+  container.appendChild(header);
   createNav();
 };
 
@@ -43,13 +57,13 @@ const createNavList = () => {
   navTitle.innerText = "Lorem ipsum";
 
   const navBody = document.createElement("div");
-  const navCount = document.createElement("h3");
+  const navCount = document.createElement("h4");
   navCount.classList = "nav-count";
-  navCount.innerText = "Count of selected cards : 0";
+  navCount.innerText = countMessage + "0";
 
-  const navSelectedColor = document.createElement("h3");
+  const navSelectedColor = document.createElement("h4");
   navSelectedColor.classList = "nav-selected-color";
-  navSelectedColor.innerText = "Selected colors : none";
+  navSelectedColor.innerText = selectedMessage + "none";
 
   nav.appendChild(navTitle);
   nav.appendChild(navBody);
@@ -60,72 +74,68 @@ const createNavList = () => {
 const createMain = () => {
   const main = document.createElement("main");
   main.classList = "cards-wrapper";
-  Container.appendChild(main);
-  createCard();
+  container.appendChild(main);
+  createCard(state.data);
 };
 
-const createCard = () => {
+const createCard = (data) => {
   const main = document.querySelector(".cards-wrapper");
   const cardsContainer = document.createElement("div");
   cardsContainer.classList = "cards-container";
   main.appendChild(cardsContainer);
-  state.data.forEach((card, i) => {
-    const cardWrapper = document.createElement("section");
-    const cardColor = document.createElement("div");
-    const cardBodyWrapper = document.createElement("section");
-    cardBodyWrapper.classList = "card-body";
-    const cardTitle = document.createElement("h3");
-    cardTitle.innerText = card.title;
-    const cardBody = document.createElement("p");
-    cardBody.innerText = card.title;
-    const cardButton = document.createElement("button");
-    cardButton.innerText = "Select";
-    cardButton.classList = "card-btn";
-    cardWrapper.classList = `card`;
-    let colorHex = card.url.slice(32);
-    cardButton.id = `card${colorHex}`;
-    cardColor.style.backgroundColor = `#${colorHex}`;
-    cardsContainer.appendChild(cardWrapper);
-    cardWrapper.appendChild(cardColor);
-    cardBodyWrapper.appendChild(cardTitle);
-    cardBodyWrapper.appendChild(cardBody);
-    cardBodyWrapper.appendChild(cardButton);
-    cardWrapper.appendChild(cardBodyWrapper);
-  });
-  buttonsSelector();
-};
+  data.length &&
+    data.forEach((card, i) => {
+      const cardBody = document.createElement("p");
+      const cardBodyWrapper = document.createElement("section");
+      const cardButton = document.createElement("button");
+      const cardColor = document.createElement("div");
+      const cardTitle = document.createElement("h3");
+      const cardWrapper = document.createElement("section");
+      cardBodyWrapper.classList = "card-body";
+      cardButton.classList = "card-btn";
+      cardWrapper.classList = `card`;
+      cardBody.innerText = card.title;
+      cardButton.innerText = "Select";
+      cardTitle.innerText = card.title;
+      cardButton.id = colorsHex[i];
+      cardButton.addEventListener("click", (e) => {
+        selectColor(e, cardButton);
+      });
+      cardButton.removeEventListener("click", (e) => {
+        selectColor(e);
+      });
 
-const buttonsSelector = () => {
-  const buttons = document.querySelectorAll(".card-btn");
-  buttons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-      selectColor(e, button);
+      cardColor.style.background = colorsHex[i];
+      cardsContainer.appendChild(cardWrapper);
+      cardWrapper.appendChild(cardColor);
+      cardBodyWrapper.appendChild(cardTitle);
+      cardBodyWrapper.appendChild(cardBody);
+      cardBodyWrapper.appendChild(cardButton);
+      cardWrapper.appendChild(cardBodyWrapper);
     });
-    button.removeEventListener("click", (e) => {
-      selectColor(e);
-    });
-  });
 };
 
 const selectColor = (e, button) => {
   const navCount = document.querySelector(".nav-count");
   const navSelected = document.querySelector(".nav-selected-color");
-  const isSelected = selectedColors.indexOf(colors[`#${e.target.id.slice(4)}`]);
+  let color = colors[e.target.id];
+  const isSelected = selectedColors.indexOf(color);
   if (isSelected == -1) {
-    selectedColors.push(colors[`#${e.target.id.slice(4)}`]);
-    navCount.innerText = `Count of selected cards : ${selectedColors.length}`;
-    button.style.backgroundColor = "red";
+    selectedColors.push(color);
+    navCount.innerText = `${countMessage} ${selectedColors.length}`;
+    button.classList.add("selected");
   } else {
     selectedColors.splice(isSelected, 1);
-    navCount.innerText = `Count of selected cards : ${selectedColors.length}`;
-    button.style.backgroundColor = "royalblue";
+    navCount.innerText = `${countMessage} ${selectedColors.length}`;
+    button.classList.remove("selected");
   }
   if (!selectedColors.length) {
-    navSelected.innerText = `Selected colors : none`;
+    navSelected.innerText = `${selectedMessage} none`;
   } else {
-    navSelected.innerText = `Selected colors : ${selectedColors.join(", ")}`;
+    navSelected.innerText = `${selectedMessage} ${selectedColors.join(", ")}`;
   }
 };
+
 const buildlUI = async () => {
   state.data = await fetchAPI();
   createHeader();
